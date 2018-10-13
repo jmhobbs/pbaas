768acbae4f80153171caf45de2948b94b207ef81
Alexander Mykolaichuk <alexmix3000@gmail.com>
2018-10-13 13:52:55 +0300

    added gRPC delete method

--- examples/grpcClient.go
+++ examples/grpcClient.go
@@ -17,23 +17,48 @@ func main() {
 
 	c := pb.NewProgressBarServiceClient(conn)
 
-	r, err := c.NewProgressBar(context.Background(), &pb.NewProgressBarRequest{5})
+	r1, err := c.NewProgressBar(context.Background(), &pb.NewProgressBarRequest{5})
 	if err != nil {
 		log.Fatalf("could not create progress bar: %v", err)
 	}
 
-	log.Println("New Progress Bar:", r)
+	log.Println("New Progress Bar:", r1)
+
+	// Get progress
+	r2, err := c.GetProgressBarStatus(context.Background(), &pb.ProgressBarStatusRequest{
+		Ids: []string{r1.GetId()},
+	})
+	if err != nil {
+		log.Fatalf("could not get progress bar: %v", err)
+	}
+
+	log.Println("Got Progress Bar:", r2)
+
 
 	// Update progress
-	res, err := c.UpdateProgressBar(context.Background(), &pb.UpdateProgressBarRequest{
-		Token: r.Token,
-		Id: r.Id,
+	r3, err := c.UpdateProgressBar(context.Background(), &pb.UpdateProgressBarRequest{
+		Token: r1.Token,
+		Id: r1.Id,
 		NewProgressValue: 20,
 	})
 	if err != nil {
 		log.Fatalf("could not update progress bar: %v", err)
 	}
 
-	log.Println("New Progress Bar Value:", res)
+	log.Println("New Progress Bar Value:", r3)
+
+	// Get progress
+	r4, _ := c.GetProgressBarStatus(context.Background(), &pb.ProgressBarStatusRequest{
+		Ids: []string{r1.GetId()},
+	})
+
+	log.Println("Got Progress Bar:", r4)
+
+	// Delete progress
+	r5, _ := c.DeleteProgressBar(context.Background(), &pb.ProgressBarStatusRequest{
+		Ids: []string{r1.GetId()},
+	})
+
+	log.Println("Deleted Progress Bar:", r5)
 
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module "https://github.com/jmhobbs/pbaas"
+module github.com/jmhobbs/pbaas
 
 require (
+	github.com/golang/protobuf v1.2.0
 	github.com/gorilla/mux v1.6.2
-	github.com/jmhobbs/pbaas v0.0.0-20181011161433-bcbbff13094f
 	github.com/pkg/errors v0.8.0
 	github.com/satori/go.uuid v1.2.0
+	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 	google.golang.org/grpc v1.15.0
 )
